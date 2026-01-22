fb478b67fe0bea56f8c9e05f9f0b87534326f4a5
Axel <contact@clairedecode.fr>
2026-01-22 23:54:49 +0100

    feat: add config.fish

---
+++ fish/config.fish
@@ -0,0 +1,13 @@
+source /usr/share/cachyos-fish-config/cachyos-config.fish
+
+fnm env --use-on-cd | source
+
+
+alias copy='xclip -selection clipboard'
+
+
+# overwrite greeting
+# potentially disabling fastfetch
+#function fish_greeting
+#    # smth smth
+#end
